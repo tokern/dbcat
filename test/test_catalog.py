b997656a296605c91d957327951c7f1f310064c6
Rajat Venkatesh <vrajat@users.noreply.github.com>
2021-01-16 10:53:18 +0530

    feature: Config support for catalog and connections

--- test/test_catalog.py
+++ test/test_catalog.py
@@ -1,3 +1,6 @@
+import yaml
+
+from dbcat.catalog import Catalog
 from dbcat.scanners.json import File
 
 
@@ -10,3 +13,22 @@ def test_file_scanner():
     default_schema = catalog.schemata[0]
     assert default_schema.name == "default"
     assert len(default_schema.tables) == 8
+
+
+postgres_conf = """
+catalog:
+  type: postgres
+  user: db_user
+  password: db_password
+  host: db_host
+  port: db_port
+"""
+
+
+def test_catalog_config():
+    config = yaml.safe_load(postgres_conf)
+    conn: Catalog = Catalog(**config["catalog"])
+    assert conn.type == "postgres"
+    assert conn.user == "db_user"
+    assert conn.host == "db_host"
+    assert conn.port == "db_port"
