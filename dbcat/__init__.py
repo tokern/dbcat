d541914b43a1c3022a12a4f8f44348244b2c698e
Rajat Venkatesh <vrajat@users.noreply.github.com>
2021-01-16 19:22:35 +0530

    feature: Add pull cli and api to scan and load catalogs

--- dbcat/__init__.py
+++ dbcat/__init__.py
@@ -1,1 +1,5 @@
+# flake8: noqa
+
 __version__ = "0.1.0"
+
+from dbcat.dbcat import pull  # type: ignore
---
+++ test/conftest.py
@@ -0,0 +1,168 @@
+from contextlib import closing
+
+import psycopg2
+import pymysql
+import pytest
+import yaml
+
+from dbcat.catalog.metadata import Connection as DbConnection
+from dbcat.catalog.orm import Connection
+
+postgres_conf = """
+catalog:
+  type: postgres
+  user: piiuser
+  password: p11secret
+  host: 127.0.0.1
+  port: 5432
+  database: piidb
+"""
+
+
+@pytest.fixture
+def root_connection():
+    config = yaml.safe_load(postgres_conf)
+    with closing(Connection(**config["catalog"])) as conn:
+        yield conn
+
+
+@pytest.fixture
+def setup_catalog(root_connection):
+    with root_connection.engine.connect() as conn:
+        conn.execute("CREATE USER catalog_user PASSWORD 'catal0g_passw0rd'")
+        conn.execution_options(isolation_level="AUTOCOMMIT").execute(
+            "CREATE DATABASE tokern"
+        )
+        conn.execution_options(isolation_level="AUTOCOMMIT").execute(
+            "GRANT ALL PRIVILEGES ON DATABASE tokern TO catalog_user"
+        )
+
+    yield root_connection
+
+    with root_connection.engine.connect() as conn:
+        conn.execution_options(isolation_level="AUTOCOMMIT").execute(
+            "DROP DATABASE tokern"
+        )
+
+        conn.execution_options(isolation_level="AUTOCOMMIT").execute(
+            "DROP USER catalog_user"
+        )
+
+
+catalog_conf = """
+catalog:
+  type: postgres
+  user: catalog_user
+  password: catal0g_passw0rd
+  host: 127.0.0.1
+  port: 5432
+  database: tokern
+"""
+
+
+@pytest.fixture
+def catalog_connection(setup_catalog):
+    config = yaml.safe_load(catalog_conf)
+    with closing(Connection(**config["catalog"])) as conn:
+        yield conn
+
+
+pii_data_script = """
+create table no_pii(a text, b text);
+insert into no_pii values ('abc', 'def');
+insert into no_pii values ('xsfr', 'asawe');
+
+create table partial_pii(a text, b text);
+insert into partial_pii values ('917-908-2234', 'plkj');
+insert into partial_pii values ('215-099-2234', 'sfrf');
+
+create table full_pii(name text, location text);
+insert into full_pii values ('Jonathan Smith', 'Virginia');
+insert into full_pii values ('Chase Ryan', 'Chennai');
+
+"""
+
+
+pii_data_load = [
+    "create table no_pii(a text, b text)",
+    "insert into no_pii values ('abc', 'def')",
+    "insert into no_pii values ('xsfr', 'asawe')",
+    "create table partial_pii(a text, b text)",
+    "insert into partial_pii values ('917-908-2234', 'plkj')",
+    "insert into partial_pii values ('215-099-2234', 'sfrf')",
+    "create table full_pii(name text, location text)",
+    "insert into full_pii values ('Jonathan Smith', 'Virginia')",
+    "insert into full_pii values ('Chase Ryan', 'Chennai')",
+]
+
+pii_data_drop = ["DROP TABLE full_pii", "DROP TABLE partial_pii", "DROP TABLE no_pii"]
+
+
+def mysql_conn():
+    return (
+        pymysql.connect(
+            host="127.0.0.1", user="piiuser", password="p11secret", database="piidb",
+        ),
+        DbConnection(
+            type="mysql",
+            uri="127.0.0.1",
+            username="piiuser",
+            password="p11secret",
+            database="piidb",
+        ),
+        "piidb",
+    )
+
+
+def pg_conn():
+    return (
+        psycopg2.connect(
+            host="127.0.0.1", user="piiuser", password="p11secret", database="piidb"
+        ),
+        DbConnection(
+            type="postgres",
+            uri="127.0.0.1",
+            username="piiuser",
+            password="p11secret",
+            database="piidb",
+            cluster="public",
+        ),
+        "public",
+    )
+
+
+@pytest.fixture(params=[mysql_conn(), pg_conn()])
+def load_data(request):
+    db_conn, extractor_conn, expected_schema = request.param
+    with closing(db_conn) as conn:
+        with conn.cursor() as cursor:
+            for statement in pii_data_load:
+                cursor.execute(statement)
+            cursor.execute("commit")
+        yield conn, extractor_conn, expected_schema
+        with conn.cursor() as cursor:
+            for statement in pii_data_drop:
+                cursor.execute(statement)
+            cursor.execute("commit")
+
+
+@pytest.fixture
+def load_all_data():
+    params = [mysql_conn(), pg_conn()]
+    for p in params:
+        db_conn, extractor_conn, expected_schema = p
+        with db_conn.cursor() as cursor:
+            for statement in pii_data_load:
+                cursor.execute(statement)
+            cursor.execute("commit")
+    yield params
+    for p in params:
+        db_conn, extractor_conn, expected_schema = p
+        with db_conn.cursor() as cursor:
+            for statement in pii_data_drop:
+                cursor.execute(statement)
+            cursor.execute("commit")
+
+    for p in params:
+        db_conn, extractor_conn, expected_schema = p
+        db_conn.close()
